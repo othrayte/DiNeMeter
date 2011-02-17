38f0c0c99ed996b6e8735ebf919cc1269bc5ef5d
Adrian Cowan (othrayte) <othrayte@gmail.com>
2011-02-17 17:43:08 +1100

    Updated source files from the sub-project WebMonitorMaster to reflect the change in license from GPLv3 to AGPLv3

--- src/webmonitor/master/frontend/RootFill.hx
+++ src/webmonitor/master/frontend/RootFill.hx
@@ -4,16 +4,16 @@ package webmonitor.master.frontend;
  *  This file is part of WebMonitorMaster.
  *
  *  WebMonitorMaster is free software: you can redistribute it and/or modify
- *  it under the terms of the GNU General Public License as published by
- *  the Free Software Foundation, either version 3 of the License, or
- *  any later version.
+ *  it under the terms of the GNU Affero General Public License as
+ *  published by the Free Software Foundation, either version 3 of the
+ *  License, or any later version.
  *
  *  WebMonitorMaster is distributed in the hope that it will be useful,
  *  but WITHOUT ANY WARRANTY; without even the implied warranty of
  *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *  GNU General Public License for more details.
+ *  GNU Affero General Public License for more details.
  *
- *  You should have received a copy of the GNU General Public License
+ *  You should have received a copy of the GNU Affero General Public License
  *  along with WebMonitorMaster.  If not, see <http://www.gnu.org/licenses/>.
  * 
  * @author othrayte
