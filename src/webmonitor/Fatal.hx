da69361c32e4bf43c6271d9b31ec5a1c9da2c2f3
Adrian Cowan (othrayte) <othrayte@gmail.com>
2011-02-17 18:45:13 +1100

    Fixed the licenses in the commited files to reflect the fact that they are from the common WebMonitor project

--- src/webmonitor/Fatal.hx
+++ src/webmonitor/Fatal.hx
@@ -1,20 +1,20 @@
 package webmonitor;
 
 /**
- *  This file is part of WebMonitorMaster.
+ *  This file is part of WebMonitor.
  *
- *  WebMonitorMaster is free software: you can redistribute it and/or modify
+ *  WebMonitor is free software: you can redistribute it and/or modify
  *  it under the terms of the GNU General Public License as published by
  *  the Free Software Foundation, either version 3 of the License, or
  *  any later version.
  *
- *  WebMonitorMaster is distributed in the hope that it will be useful,
+ *  WebMonitor is distributed in the hope that it will be useful,
  *  but WITHOUT ANY WARRANTY; without even the implied warranty of
  *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  *  GNU General Public License for more details.
  *
  *  You should have received a copy of the GNU General Public License
- *  along with WebMonitorMaster.  If not, see <http://www.gnu.org/licenses/>.
+ *  along with WebMonitor.  If not, see <http://www.gnu.org/licenses/>.
  *
  * @author Adrian Cowan (othrayte)
  */
